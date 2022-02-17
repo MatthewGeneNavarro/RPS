b2cafe565d219c805d562f40a9ad5242f6c9c52b
MatthewGeneNavarro <matthewgenenavarro@protonmail.com>
2022-02-17 11:22:31 -0600

    adding ui

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
+
+let playerWins = 0;
+let computerWins = 0;
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissor = document.querySelector('.scissor');
+let player = '';
+rock.addEventListener('click', () =>{
+    player = 'rock';
+    console.log("Inside rock event listener");
+    game(computerPlay() ,player);
+    
+})
+
+paper.addEventListener('click',()=> {
+    player = 'paper';
+    console.log("Inside rock event listener");
+    game(computerPlay() ,player);
+})
+
+scissor.addEventListener('click',()=> {
+    player = 'scissors';
+    console.log("Inside rock event listener");
+    game(computerPlay() ,player);
+})
+
+
+//computer choice output = 'rock' || 'paper' || 'scissor'
 function computerPlay() {
+    console.log('Inside computerPlay function');
     const min = 1;
     const max = 3;
     let sum = Math.floor(Math.random() * (max - min +1) + min);
@@ -10,45 +39,96 @@ function computerPlay() {
     } else if (sum === 3) {
         return "scissors";
     }
-    
+
 }
 
+
+// takes  player choice string && computer choice string as input
 function playRound(playerSelection, computerSelection) {
 
+    console.log('Inside playRound function');
      playerSelection = playerSelection.toLowerCase();
     if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("Computer wins! Paper beats Rock");
+        let x = document.createElement('p');
+        x.textContent = "Computer wins! Paper beats Rock"
+        document.querySelector('.results .winner').append(x);
         return 2;
     } else if (computerSelection === "rock" && playerSelection === "paper") {
-        console.log("Player wins! Paper beats Rock");
+        let x = document.createElement('p');
+        x.textContent = "Player wins! Paper beats Rock"
+        document.querySelector('.results .winner').append(x);
         return 1;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("Player wins! Rock beats Scissors");
+        let x = document.createElement('p');
+        x.textContent = "Player wins! Rock beats Scissors"
+        document.querySelector('.results .winner').append(x);
         return 1;
     } else if (computerSelection === "rock" && playerSelection === "scissors") {
-        console.log("Computer wins! Rock beats Scissors");
+        let x = document.createElement('p');
+        x.textContent = "Computer wins! Rock beats Scissors"
+        document.querySelector('.results .winner').append(x);
         return 2;
     } else if ( playerSelection === "paper" && computerSelection === "scissors") {
-        console.log("Computer wins! Scissors beat Paper");
+        let x = document.createElement('p');
+        x.textContent = "Computer wins! Scissors beat Paper"
+        document.querySelector('.results .winner').append(x);
         return 2;
     } else if (computerSelection === "paper" && playerSelection === "scissors") {
-        console.log("Player wins! Scissors beat Paper");
+        let x = document.createElement('p');
+        x.textContent = "Player wins! Scissors beat Paper"
+        document.querySelector('.results .winner').append(x);
         return 1;
-    }  
-
-}
-
-function game() {
-    let player = 0;
-    let computer = 0;
-
-    for(x =0; x<= 5; x++){
-        y = playRound(prompt("Player choose"), computerPlay());
-        if (y === 1){
-            player ++;
-        } else if (y === 2) {
-            computer ++;
-        }
+    }  else {
+        let x = document.createElement('p');
+        x.textContent = "Tie"
+        document.querySelector('.results .winner').append(x);
+        return 0;
     }
- 
+
+
 }
+
+
+// two inputs 
+// results computerChoice, playerChoice
+// end game when a winner has 5 points
+function game( computerPlay, playerchoice) {
+
+    console.log('inside game function');
+    console.log(computerPlay);
+    console.log(playerchoice);
+
+
+
+    y = playRound(playerchoice, computerPlay);
+    
+    if (y === 1){
+        if (playerWins === 5){
+            let winner = document.createElement('p');
+            winner.textContent = "Player won the Game!";
+            document.querySelector('.winner').append(winner);
+        }
+        playerWins ++;
+        document.querySelector('#player').textContent = playerWins;
+    } else if (y === 2) {
+        if (computerWins === 5){
+            let winner = document.createElement('p');
+            winner.textContent = "Computer won the Game!";
+            document.querySelector('.winner').append(winner);
+        }
+        computerWins ++;
+        document.querySelector('#computer').textContent = computerWins;
+    } 
+
+    console.log(computerWins);
+    console.log(playerWins);
+
+    //remove loop
+    //check Wins on each function execution
+ 
+
+}
+
+/* let x = document.createElement('p');
+x.textContent = "test";
+document.querySelector('.results').append(x); */
